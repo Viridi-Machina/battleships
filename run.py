3bffb959e7c068c0a932c5327dd71f5e2565791b
jacen green <jacen.l.green@hotmail.com>
2024-02-01 22:52:19 +0000

    feat: create function using code that displays the grid

--- run.py
+++ run.py
@@ -19,6 +19,8 @@
 # Track score and save user email for login, store score and user information on external sheet
 # With enough time, allow import from external sheet (with link for users) to import ship placement
 
+from colorama import Fore
+
 grid_size = 10 # Default grid-size; future implementation to alter based on user choice
 
 # Unicode characters used for border styling
@@ -29,25 +31,39 @@ tl_corner = '\u250F'
 tr_corner = '\u2513'
 br_corner = '\u251B'
 bl_corner = '\u2517'
+hit = '\U0001F7D2  '
+miss = '\U0001F7AC  '
 
-# '\U0001F7AC  ' miss
-# '\U0001F7D2  ' hit
-print(f'\n  {tl_corner}{line_x}{tr_corner}')
-grid_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+def display_grid(hits,misses):
+    '''
+    This function displays the updated grid for the player; 
+    showing any hits and misses on the board.
 
+    Two for loops create a grid that scales based on the defined grid_size variable.
+    'grid_row = grid_row + grid_element' adapted from python code from a tutorial video 
+    from 'Dr Codie' on YouTube: https://www.youtube.com/@DrCodie
+    '''
+    # Forms grid top
+    print(f'\n  {tl_corner}{line_x}{tr_corner}')
+    grid_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
-# Two for loops create a grid that scales based on the defined grid_size variable.
-# 'grid_row = grid_row + grid_element' adapted from python code from a tutorial video 
-# from 'Dr Codie' on YouTube: https://www.youtube.com/@DrCodie
+    # Forms grid body
+    grid_location = 11 # Starts at 11 as the grid numbering starts from 1,1 (A1)
+    for x in range(grid_size):
+        grid_row = ''
+        for y in range(grid_size):
+            grid_element = '\U0001F785  ' # Circle unicode character with space
+            if grid_location in misses:
+                grid_element = miss
+            elif grid_location in hits:
+                grid_element = hit
+            grid_row = grid_row + grid_element
+            grid_location = grid_location + 1
+        print(grid_letters[x], line_y, grid_row, line_y)
 
-grid_location = 11 # Starts at 11 as the grid numbering starts from 1,1 (A1)
-for x in range(grid_size):
-    grid_row = ''
-    for y in range(grid_size):
-        grid_element = '\U0001F785  ' # Circle unicode character with space
-        if grid_location == 35:
-            grid_element = '\U0001F7D2  '
-        grid_row = grid_row + grid_element
-        grid_location = grid_location + 1
-    print(grid_letters[x], line_y, grid_row, line_y)
-print(f'  {bl_corner}{line_x}{br_corner}\n     1  2  3  4  5  6  7  8  9  10\n')
+    # Forms grid bottom
+    print(f'  {bl_corner}{line_x}{br_corner}\n     1  2  3  4  5  6  7  8  9  10\n')
+
+hits = [11,21,31]
+misses = [13,14,15]
+display_grid(hits,misses)
