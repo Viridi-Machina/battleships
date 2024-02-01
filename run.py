ce578f403c794993afe9e17b4d76ba42faaaac39
jacen green <jacen.l.green@hotmail.com>
2024-02-01 16:54:44 +0000

    planning: add initial comments to follow coding

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# FUNCTION Launch Game
+# Welcome Screen
+# INPUT Enter UserName
+# Store UserName
+# SELECT BOARD SIZE
+# Show Board
+# SELECT SHIP PLACEMENT
+# Show Board
+
+# START BATTLESHIPS
+# Show Board
+# INPUT Enter Guess
+# VALIDATE Guess
+# Guess already taken?
+# - Within board scope?
+# - Hit / miss / sink?
+# - Offer feedback?
+
+# Track score and save user email for login, store score and user information on external sheet
+# With enough time, allow import from external sheet (with link for users) to import ship placement
+
